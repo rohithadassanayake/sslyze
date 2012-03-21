564bce8653d101a2349ad23eba3744858f0d99e7
nabla.c0d3 <nabla.c0d3@b809e1dc-774c-e543-caea-553a5087e510>
2012-03-21 05:38:20 +0000

    Releasing current code as SSLyze v0.4

---
+++ tags/release-0.4/utils/ctSSL/features_not_available.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#-------------------------------------------------------------------------------
+# Name:         features_not_available.py
+# Purpose:      Constants defined during ctSSL's initialization. 
+#               Depending on the OpenSSL library that was loaded, specific 
+#               features might be unavailable.
+#
+# Author:       alban
+#
+# Copyright:    2011 Alban Diquet
+# License:      ctSSL is licensed under the terms of the MIT License.
+#-------------------------------------------------------------------------------
+
+SSL2_NOT_AVAIL =                        False # SSL2 support.
+SSL_SECURE_RENEGOTIATION_NOT_AVAIL =    False # Secure renegotiation APIs
+TLS1_1_TLS1_2_NOT_AVAIL =               False # TLS 1.1 and 1.2
